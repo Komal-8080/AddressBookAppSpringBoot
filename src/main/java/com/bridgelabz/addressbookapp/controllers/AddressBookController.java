234bc81babfa42ff0b870f56f8dece70a429c463
Komal-8080 <shindeg163@gmail.com>
2021-03-10 14:41:28 +0530

    ADDED: Services Layer To Store Address Book Data

--- src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
@@ -20,32 +20,28 @@ public class AddressBookController {
 
     @RequestMapping(value = {"","/","/get"})
     public ResponseEntity<ResponseDTO> getAddressBookData() {
-        List<AddressBookData> addressBookDataList = null;
-        addressBookDataList = addressBookServiceInterface.getAddressBookData();
+        List<AddressBookData> addressBookDataList = addressBookServiceInterface.getAddressBookData();
         ResponseDTO respDTO = new ResponseDTO("Get Call Sucess", addressBookDataList);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @GetMapping("/get/{personId}")
     public ResponseEntity<ResponseDTO> getAddressBookDataById(@PathVariable("personId") int personId) {
-        AddressBookData addressBookData = null;
-        addressBookData = addressBookServiceInterface.getAddressBookDataById(personId);
+        AddressBookData addressBookData = addressBookServiceInterface.getAddressBookDataById(personId);
         ResponseDTO respDTO = new ResponseDTO("Get Call Success for personId: "+personId, addressBookData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
-        AddressBookData addressBookData = null;
-        addressBookData = addressBookServiceInterface.addAddressBookData(addressBookDTO);
+        AddressBookData addressBookData = addressBookServiceInterface.addAddressBookData(addressBookDTO);
         ResponseDTO respDTO = new ResponseDTO( "Created Address Book Data for: " +addressBookDTO,addressBookData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @PutMapping("/update/{personId}")
     public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("personId") int personId ,@RequestBody AddressBookDTO addressBookDTO) {
-        AddressBookData addressBookData = null;
-        addressBookData = addressBookServiceInterface.updateAddressBookData(addressBookDTO);
+        AddressBookData addressBookData = addressBookServiceInterface.updateAddressBookData(personId,addressBookDTO);
         ResponseDTO respDTO = new ResponseDTO( "Updated Address Book Data For: "+addressBookDTO,addressBookData);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
--- src/main/java/com/bridgelabz/addressbookapp/services/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/services/AddressBookService.java
@@ -10,36 +10,37 @@ import java.util.List;
 @Service
 public class AddressBookService implements AddressBookServiceInterface {
 
+    private List<AddressBookData> addressBookDataList = new ArrayList<>();
 
     @Override
     public List<AddressBookData> getAddressBookData() {
-        List<AddressBookData> addressBookDataList = new ArrayList<>();
-        addressBookDataList.add(new AddressBookData(1, new AddressBookDTO("Sudhir", "Shinde")));
         return addressBookDataList;
     }
 
     @Override
     public AddressBookData getAddressBookDataById(int personId) {
-        AddressBookData addressBookData = null;
-        addressBookData = new AddressBookData(1, new AddressBookDTO("Sudhir","Shinde"));
-        return addressBookData;
+        return addressBookDataList.get(personId-1);
     }
 
     @Override
     public AddressBookData addAddressBookData(AddressBookDTO addressBookDTO) {
         AddressBookData addressBookData = null;
-        addressBookData = new AddressBookData(1, addressBookDTO);
+        addressBookData = new AddressBookData(addressBookDataList.size()+1, addressBookDTO);
+        addressBookDataList.add(addressBookData);
         return addressBookData;
     }
 
     @Override
-    public AddressBookData updateAddressBookData(AddressBookDTO addressBookDTO) {
-        AddressBookData addressBookData = null;
-        addressBookData = new AddressBookData(1, addressBookDTO);
+    public AddressBookData updateAddressBookData(int personId,AddressBookDTO addressBookDTO) {
+        AddressBookData addressBookData = this.getAddressBookDataById(personId);
+        addressBookData.setFirstName(addressBookDTO.firstName);
+        addressBookData.setLastName(addressBookDTO.lastName);
+        addressBookDataList.set(personId-1,addressBookData);
         return addressBookData;
     }
 
     @Override
     public void deleteAddressBookData(int personId) {
+        addressBookDataList.remove(personId-1);
     }
 }
