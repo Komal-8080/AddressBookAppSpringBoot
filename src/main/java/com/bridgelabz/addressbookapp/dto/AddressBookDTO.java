8c95992996fe45e59a9970637781c0841f7efd4c
Komal-8080 <shindeg163@gmail.com>
2021-03-12 00:39:48 +0530

    ADDED: User Friendly Error Response Added

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -4,9 +4,9 @@ import javax.validation.constraints.Pattern;
 
 public class AddressBookDTO {
 
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "First Name Invalid")
     public String firstName;
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Last Name Invalid")
     public String lastName;
 
     public AddressBookDTO(String firstName, String lastName) {
